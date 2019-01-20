86c685a096572c8fb4999054e38ac8f22790d40e
Aimanali48 <aimanaly48@gmail.com>
2019-01-20 23:37:21 +0500

    added modals

--- client/src/components/recipes.js
+++ client/src/components/recipes.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 class Recipes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(activeIndex) {
+    this.setState({
+      modal: !this.state.modal,
+      activeModalIndex: activeIndex
+    });
+  }
+
   render() {
     //  console.log(this.props.data)
     // console.log(this.props.onDelete)
-
     return (
       <div>
         <h2 className="jumbotron heading">Recipes</h2>
@@ -25,8 +41,21 @@ class Recipes extends Component {
               Recipe recommended by : &nbsp; <strong>{c.name}</strong>
               <br />
               Recipe Name:&nbsp; <strong>{c.rcpname}</strong> <br />
-              <strong>Here's the recipe</strong>
-              <hr /> {c.recipe}
+              <hr />
+              <div id="modal">
+                <Button color="info" onClick={() => this.toggle(index)}>Here's your recipe</Button>
+                <Modal isOpen={(this.state.activeModalIndex === index) && this.state.modal} toggle={this.toggle} className={this.props.className}>
+                  <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                  <ModalBody>
+                    {c.recipe}
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>Yumm!</Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+
             </div>
           );
         })}
